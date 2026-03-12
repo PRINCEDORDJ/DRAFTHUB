a29cd83ea0efb0a56a3a7fbb3c4d8835898acdca
PRINCEDORDJ <kinlion00@gmail.com>
2026-03-12 01:34:40 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { MailProvider } from "./context/MailContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
     <MailProvider>
       <App />
     </MailProvider>
-  </StrictMode>,
+  ,
 );
